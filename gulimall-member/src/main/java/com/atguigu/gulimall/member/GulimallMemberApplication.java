ebad93e4fdd060418519465e28d71fd020ccca7a
waterplants <1017834057@qq.com>
2024-05-07 18:15:39 +0800

    :sparkles: nacos注册中心和配置中心设置

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -4,8 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @MapperScan("com.atguigu.gulimall.member.dao")
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GulimallMemberApplication {
     public static void main(String[] args) {
--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.atguigu.gulimall.ware.dao")
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GulimallWareApplication {
     public static void main(String[] args) {
